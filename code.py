0ed427ea38a6482cf74538ddb5caa8606dac1065
Blent User <user@blent>
2024-06-14 12:49:37 +0000

    Updated message

--- code.py
+++ code.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 
-print("Hello ! Il est exactement {}.".format(datetime.now().strftime("%H:%M:%S")))
+print("Hello Blent ! Il est exactement {}.".format(datetime.now().strftime("%H:%M:%S")))
